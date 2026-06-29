ff7a433124eed87c50d23e7131e62baab05a2446
Lutz Rohlfing <287668484+lutzr2000@users.noreply.github.com>
2026-06-29 13:38:18 +0200

    rescaling of forces

--- Solver/Kernel_GPU/advection_schemes.py
+++ Solver/Kernel_GPU/advection_schemes.py
@@ -640,9 +640,9 @@ def update_velocity_maccormack(
 
 
     # Constant force
-    Fx += fx_const
-    Fy += fy_const
-    Fz += fz_const
+    Fx += fx_const * 10
+    Fy += fy_const * 10
+    Fz += fz_const * 10
 
     # Buoyancy
     Fz += buoyancy_approximation(
